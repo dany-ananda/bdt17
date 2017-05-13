851a86f759e555e6bc30a70189855a4e5ff28c1c
dany-ananda <dany.ananda@gmail.com>
2017-05-13 14:43:54 +0800

    add js

---
+++ js/satu.js
@@ -0,0 +1,17 @@
+/*
+var names = [];
+names[0] = "Mahmud";
+names[1] = "Ismail";
+names[2] = "Habibie";
+
+for (i=0; i<3; i++) {
+	console.log(names[i]);
+}
+*/
+/*
+var cats = ['kitty','casper','bony'];
+cats.forEach(function(item) {
+	console.log('meow ' + item);
+}
+);
+*/
