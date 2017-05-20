2a56ce3133f224d4909f72af58ad2a78446a7b33
dany-ananda <dany.ananda@gmail.com>
2017-05-20 16:24:53 +0800

    add module.js

---
+++ js/module.js
@@ -0,0 +1,41 @@
+
+var fs = require('fs');
+var path = require('path');
+var folder = process.argv[2];
+
+module.exports = function(dirName, extName, callback) {
+    fs.readdir(dirName, function(err, files) {
+        var _ext = '.' + extName;
+
+        if(err) {
+            callback(err, null);
+        } else {
+            var filteredFile = [];
+            for(var i=1; i<files.length; i++) {
+                 if(_ext == path.extname(files[i])) {
+                    filteredFile.push(files[i]);
+                 } else {
+                     continue;
+                 }
+            }
+        callback(err, filteredFile);
+        }
+    });
+}
+
+//OR
+/*
+    module.exports = function (dir, filterStr, callback) {
+      fs.readdir(dir, function (err, list) {
+        if (err) {
+          return callback(err)
+        }
+
+        list = list.filter(function (file) {
+          return path.extname(file) === '.' + filterStr
+        })
+
+        callback(null, list)
+      })
+    }
+*/
